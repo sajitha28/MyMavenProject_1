971a4ae8c5c54d0702d9f93333a206dd8b739cbc
sajit <sajit@LAPTOP-L3IGT96C>
2023-07-24 23:08:42 -0400

    my first commit

---
+++ MyMavenProject_1/src/main/java/Sample/DrivingLicenseEligibility.java
@@ -0,0 +1,21 @@
+package Sample;
+
+public class DrivingLicenseEligibility {
+	static int age=15;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub static int age=15;
+	
+			if(age>=18)
+			{
+				System.out.println("Eligible for Driving license");
+			}
+			else
+			{
+				System.out.println("Not eligible for drivig license");
+			}
+			
+		}
+
+	}
+
+
---
+++ MyMavenProject_1/src/main/java/Sample/SwitchVowels.java
@@ -0,0 +1,34 @@
+package Sample;
+
+public class SwitchVowels {
+	
+	public static void main(String args[])
+	{
+	char vowel='B';
+	switch(vowel)
+	{
+		case 'a':
+		System.out.println("char is  a vowel");
+		break;// to stop flow of prgm
+		case 'e':
+		System.out.println(" char is a vowel");
+		break;
+		case 'i':
+		System.out.println("char is a vowel");
+		break;
+		case 'o':
+		System.out.println("char is a vowel");
+		break;
+		case 'u':
+		System.out.println("char is a vowel");
+		break;
+		default:
+		System.out.println("NOT A VOWEL");
+	}
+
+	}
+	}
+
+	
+
+
